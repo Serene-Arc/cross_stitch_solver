8de3b34f542fb61cb64efdf18bd64f6902255bc3
Serene-Arc <33189705+Serene-Arc@users.noreply.github.com>
2023-11-21 11:24:26 +1000

    Add tests

--- src/affixed_permutations.rs
+++ src/affixed_permutations.rs
@@ -44,7 +44,7 @@ impl<T: Clone + Hash + PartialEq + Eq> Iterator for PrefixedPermutations<T> {
 #[cfg(test)]
 mod test {
     use crate::csv_reader::generate_permutations;
-    use crate::stitch::{HalfStitch, Location};
+    use crate::stitch::{make_full_stitch, HalfStitch, Location};
 
     #[test]
     fn test_permutation_generation_first_element_consistent() {
@@ -58,5 +58,74 @@ mod test {
         for p in perms {
             assert_eq!(p[0], first);
         }
+    }
+
+    #[test]
+    fn test_permutation_generation_first_element_not_consistent() {
+        let test = vec![
+            HalfStitch::new(Location::new(1, 1), true),
+            HalfStitch::new(Location::new(2, 2), true),
+            HalfStitch::new(Location::new(3, 1), true),
+        ];
+        let perms = generate_permutations(None, test.clone());
+
+        let mut is_different_first_elem_found = false;
+        for perm in perms {
+            if &perm[0] != &test[0] {
+                is_different_first_elem_found = true;
+                break;
+            }
+        }
+        assert!(
+            is_different_first_elem_found,
+            "All permutations start with the same element"
+        );
+    }
+
+    #[test]
+    fn test_permutation_generation_specific_permutation_base() {
+        let test = vec![
+            HalfStitch::new(Location::new(1, 1), true),
+            HalfStitch::new(Location::new(2, 2), true),
+            HalfStitch::new(Location::new(3, 1), true),
+        ];
+        let perms = generate_permutations(None, test.clone());
+
+        let mut found = false;
+        for perm in perms {
+            if perm == test {
+                found = true;
+                break;
+            }
+        }
+        assert!(found, "Expected permutation was not found");
+    }
+
+    #[test]
+    fn test_permutation_generation_specific_permutation_kick() {
+        let test = [
+            make_full_stitch(1, 1),
+            make_full_stitch(2, 1),
+            make_full_stitch(3, 2),
+        ]
+        .concat();
+        let perms = generate_permutations(None, test.clone());
+
+        let expected = vec![
+            HalfStitch::new(Location::new(1, 1), true),
+            HalfStitch::new(Location::new(2, 1), true),
+            HalfStitch::new(Location::new(3, 1), false),
+            HalfStitch::new(Location::new(3, 2), true),
+            HalfStitch::new(Location::new(4, 2), false),
+            HalfStitch::new(Location::new(2, 1), false),
+        ];
+        let mut found = false;
+        for perm in perms {
+            if perm == expected {
+                found = true;
+                break;
+            }
+        }
+        assert!(found, "Expected permutation was not found");
     }
 }
